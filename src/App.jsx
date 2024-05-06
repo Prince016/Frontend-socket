122c7b42dc26f80b04575cbc4f43af23b07cf849
Prince016 <prashantsharma3854@gmail.com>
2024-05-07 00:43:42 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,79 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const App = () => {
-  const socket = io("http://localhost:3000");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketId] = useState("");
+
+  const socket = useMemo(
+    () =>
+      io("http://localhost:3000", {
+        withCredentials: true,
+      }),
+    []
+  );
 
   useEffect(() => {
     socket.on("connect", () => {
+      setSocketId(socket.id);
       console.log("connected ----> ", socket.id);
+    });
+
+    socket.on("recieve-msg", (s) => {
+      console.log("recieve msg -> ", s);
     });
 
     socket.on("welcome", (data) => {
       console.log(data);
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
-  return <div>App</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+    setMessage("");
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h6" component="div" gutterBottom>
+        Welcome to the room {socketID}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+    </Container>
+  );
 };
 
 export default App;
